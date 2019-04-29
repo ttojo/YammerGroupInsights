0eccab5eb50e4fea1a439331e6afea4bd5798001
Toshio Tojo <toshio.tojo@live.jp>
2019-04-29 18:39:57 +0900

    REST API 呼び出しメソッドを追加した。

--- Runbooks/Get-YammerGroupInsightV2.ps1
+++ Runbooks/Get-YammerGroupInsightV2.ps1
@@ -5,3 +5,53 @@ function Get-DeveloperToken {
     $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($credential.Password)
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 }
+
+function Invoke-RestAPI {
+    param (
+        [Parameter(Mandatory = $true)] [Uri] $Uri,
+        [Parameter(Mandatory = $false)] [System.Collections.IDictionary] $Headers,
+        [Parameter(Mandatory = $false)] [Microsoft.PowerShell.Commands.WebRequestMethod] $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
+        [Parameter(Mandatory = $false)] [Int32] $RetryCount = 0,
+        [Parameter(Mandatory = $false)] [Int32] $RetryInterval = 10
+    )
+
+    $completed = $false
+    $retries = 0
+
+    while (-not $completed) {
+        try {
+            $response = Invoke-RestMethod -Uri $uri -Headers $Headers -Method $Method
+            $completed = $true
+        } catch {
+            $ex = $_.Exception
+
+            if ($ex.Response -ne $null) {
+                
+                if ($retries -lt $RetryCount) {
+                    $sc = [int]$ex.Response.StatusCode.Value__
+                    if (($sc -eq 304) -or ((400 -le $sc) -and ($sc -le 599))) {
+                        $retries++
+                        Write-Verbose "Retry...($retries/$RetryCount)"
+                        Start-Sleep -Seconds $RetryInterval
+                        continue
+                    }
+                }
+                $errorResponse = $ex.Response.GetResponseStream()
+                $reader = New-Object System.IO.StreamReader($errorResponse)
+                $reader.BaseStream.Position = 0
+                $reader.DiscardBufferedData()
+                $responseBody = $reader.ReadToEnd();
+
+                Write-Host "Response content:`n$responseBody" -f Red
+                Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+            } else {
+                Write-Host "Unhanded exception:`n$ex"
+            }
+
+            #re-throw
+            throw
+        }
+    }
+
+    return $response
+}
