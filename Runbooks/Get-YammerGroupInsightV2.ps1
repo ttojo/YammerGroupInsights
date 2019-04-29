3bba9218ade1249425c65140533599c7b2a4f2f1
Toshio Tojo <toshio.tojo@live.jp>
2019-04-29 22:29:49 +0900

    グループ単体の処理に変更した。

--- Runbooks/Get-YammerGroupInsightV2.ps1
+++ Runbooks/Get-YammerGroupInsightV2.ps1
@@ -1,4 +1,8 @@
-﻿$VerbosePreference = 'Continue'
+﻿param (
+    [Parameter(Mandatory = $true)] $GroupId
+)
+
+$VerbosePreference = 'Continue'
 
 function Get-DeveloperToken {
     $credential = Get-AutomationPSCredential -Name "YammerDeveloper"
@@ -193,62 +197,62 @@ $authHeader = @{
     'Authorization' = "Bearer $developerToken"
 }
 
-Write-Verbose "対象の Yammer グループ IDを取得"
-$groupIdsString = Get-AutomationVariable -Name 'YammerGroupIds'
-Write-Verbose "Yammer グループ リスト: $groupIdsString"
-$groupIds = $groupIdsString -split ","
-
-$groupList = @()
 $messageList = @()
 $likedList = @()
 
-foreach ($groupId in $groupIds) {
+Write-Verbose "グループ (ID=$groupId) の処理を始めます。"
+$groupInfo = Get-GroupInfo -GroupId $groupId
+Write-Verbose "グループ名は [$($groupInfo.full_name)] です。"
 
-	Write-Verbose "グループ (ID=$groupId) の処理を始めます。"
-	$groupInfo = Get-GroupInfo -GroupId $groupId
-	$groupList += $groupInfo
-	Write-Verbose "グループ名は [$($groupInfo.full_name)] です。"
+Write-Host "メンバー リストを作成します。"
+$groupMembers = Get-GroupMembers -GroupId $groupId
 
-	Write-Host "メンバー リストを作成します。"
-	$groupMembers = Get-GroupMembers -GroupId $groupId
+Write-Verbose "スレッド一覧を作成します。"
+$threadMessages = Get-GroupThreads -GroupId $groupId
+Write-Verbose "トータル $($threadMessages.length) 件のメッセージを取得しました。"
 
-	Write-Verbose "スレッド一覧を作成します。"
-    $threadMessages = Get-GroupThreads -GroupId $groupId
-    Write-Verbose "トータル $($threadMessages.length) 件のメッセージを取得しました。"
+$threadMessages2 = $threadMessages | Sort-Object thread_id -Unique
+Write-Verbose "$($threadMessages2.length) 件のスレッドが見つかりました。"
 
-    $threadMessages2 = $threadMessages | Sort-Object thread_id -Unique
-    Write-Verbose "$($threadMessages2.length) 件のスレッドが見つかりました。"
-
-	foreach ($thread in $threadMessages2) {
-		$messages = Get-ThreadMessages -ThreadId $thread.thread_id
-		foreach ($message in $messages) {
-			if ($message.liked_by.count -gt 0) {
-				$likedUsers = Get-LikedUsers -MessageId $message.id
-				#foreach ($likedUser in $likedUsers) {
-				#	$likedList += [PSCustomObject]@{
-				#		message_id = $message.id
-				#		liked_user = $likedUser
-				#	}
-				#}
-			}
+foreach ($thread in $threadMessages2) {
+	$messages = Get-ThreadMessages -ThreadId $thread.thread_id
+	foreach ($message in $messages) {
+		if ($message.liked_by.count -gt 0) {
+			$likedUsers = Get-LikedUsers -MessageId $message.id
+			#foreach ($likedUser in $likedUsers) {
+			#	$likedList += [PSCustomObject]@{
+			#		message_id = $message.id
+			#		liked_user = $likedUser
+			#	}
+			#}
 		}
-		$messageList += $messages
 	}
+	$messageList += $messages
 }
 
-Write-Verbose "ファイルに保存します。"
 $LocalTargetDirectory = "C:\"
 $Date = Get-Date -Format "yyyyMMdd-HHmmss"
 
-$DateBlobName = "YammerGroup_" + $Date + ".json"
-$LocalFile = $LocalTargetDirectory + $DateBlobName
-$groupList | ConvertTo-Json -Depth 10 -Compress | Out-File -Encoding utf8 -FilePath $LocalFile
-Write-Verbose "$($LocalFile) に保存しました。"
-
-Write-Verbose "Azure ストレージに保存します。"
 $conn = Get-AutomationConnection -Name "AzureRunAsConnection"
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Out-Null
 Set-AzureRmCurrentStorageAccount -ResourceGroupName 'TOTOJO-STU-RG' -StorageAccountName 'yammergroupinsight' | Out-Null
 
+Write-Verbose "ファイルに保存します。"
+$DateBlobName = "YammerMembers_" + $GroupId + "_" + $Date + ".json"
+$LocalFile = $LocalTargetDirectory + $DateBlobName
+$groupMembers | ConvertTo-Json -Depth 10 -Compress | Out-File -Encoding utf8 -FilePath $LocalFile -Force
+Write-Verbose "$($LocalFile) に保存しました。"
+
+Write-Verbose "Azure ストレージに保存します。"
+Set-AzureStorageBlobContent -File $LocalFile -Container "json" -Blob $DateBlobName | Out-Null
+Write-Verbose "Azure ストレージに保存しました。"
+
+Write-Verbose "ファイルに保存します。"
+$DateBlobName = "YammerMessages_" + $GroupId + "_" + $Date + ".json"
+$LocalFile = $LocalTargetDirectory + $DateBlobName
+$messageList | ConvertTo-Json -Depth 10 -Compress | Out-File -Encoding utf8 -FilePath $LocalFile -Force
+Write-Verbose "$($LocalFile) に保存しました。"
+
+Write-Verbose "Azure ストレージに保存します。"
 Set-AzureStorageBlobContent -File $LocalFile -Container "json" -Blob $DateBlobName | Out-Null
 Write-Verbose "Azure ストレージに保存しました。"
