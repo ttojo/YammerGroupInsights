2baa20878b6b67123e498870440b0cb7b6d3649e
Toshio Tojo <toshio.tojo@live.jp>
2019-04-29 18:36:02 +0900

    第 2 弾向けのファイルを新規追加してみる。

---
+++ Runbooks/Get-YammerGroupInsightV2.ps1
@@ -0,0 +1,7 @@
+﻿$VerbosePreference = 'Continue'
+
+function Get-DeveloperToken {
+    $credential = Get-AutomationPSCredential -Name "YammerDeveloper"
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($credential.Password)
+    [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+}
