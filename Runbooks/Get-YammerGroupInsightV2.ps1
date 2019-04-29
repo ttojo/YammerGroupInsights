4901a2d215cadbbbe27d651beba6d41abea44b3f
Toshio Tojo <toshio.tojo@live.jp>
2019-04-29 19:15:50 +0900

    グループ リストの取得だけを実装してみた。

--- Runbooks/Get-YammerGroupInsightV2.ps1
+++ Runbooks/Get-YammerGroupInsightV2.ps1
@@ -6,6 +6,7 @@ function Get-DeveloperToken {
     [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
 }
 
+# REST API の呼び出し（リトライあり）
 function Invoke-RestAPI {
     param (
         [Parameter(Mandatory = $true)] [Uri] $Uri,
@@ -69,3 +70,138 @@ function Get-GroupInfo {
     $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method 'Get' -RetryCount 10 -RetryInterval 30
     $response
 }
+
+# グループのメンバーを列挙
+function Get-GroupMembers {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] $GroupId
+    )
+
+    $page = 1
+
+    $apiVersion = "v1"
+    $Resource = "users/in_group/$($GroupId).json"
+    $uri = "https://www.yammer.com/api/$apiVersion/$Resource"
+    $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+    $hasNext = $response.more_available
+    $members = $response.users
+
+    while ($hasNext) {
+        $page++
+        $queryParams = "page=$($page)"
+        $uri = "https://www.yammer.com/api/$apiVersion/$($Resource)?$queryParams"
+        $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+	    $hasNext = $response.more_available
+        $members += $response.users
+    }
+
+    $members
+}
+
+# グループのスレッドを列挙
+function Get-GroupThreads {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] $GroupId
+    )
+
+    $progress = 0
+    $apiVersion = "v1"
+    $Resource = "messages/in_group/$($GroupId).json"
+    $uri = "https://www.yammer.com/api/$apiVersion/$($Resource)"
+    $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+    $hasNext = $response.meta.older_available
+    $threads = $response.messages
+
+    while ($hasNext) {
+        $progress++
+        if (($progress % 20) -eq 0) {
+            Write-Verbose "$($progress) 件のメッセージを処理しました。"
+        }
+        $queryParams = "older_than=" + $response.messages[$response.messages.length - 1].id
+        $uri = "https://www.yammer.com/api/$apiVersion/$($Resource)?$queryParams"
+        $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+	    $hasNext = $response.meta.older_available
+        $threads += $response.messages
+    }
+
+    $threads
+}
+
+# スレッドのメッセージを列挙
+function Get-ThreadMessages {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] $ThreadId
+    )
+
+    $apiVersion = "v1"
+    $Resource = "messages/in_thread/$($ThreadId).json"
+    $queryParams = "threaded=true"
+    $uri = "https://www.yammer.com/api/$apiVersion/$Resource"
+    $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+    $hasNext = $response.meta.older_available
+    $messages = $response.messages
+
+    while ($hasNext) {
+        $queryParams = "older_than=" + $response.messages[$response.messages.length - 1].id
+        $uri = "https://www.yammer.com/api/$apiVersion/$($Resource)?$queryParams"
+        $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+        $hasNext = $response.meta.older_available
+        $messages += $response.messages
+    }
+
+    $messages
+}
+
+# いいねしたユーザーを列挙
+function Get-LikedUsers {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)] $MessageId
+    )
+
+    $page = 1
+
+    $apiVersion = "v1"
+    $Resource = "users/liked_message/$($MessageId).json"
+    $uri = "https://www.yammer.com/api/$apiVersion/$Resource"
+    $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+    $hasNext = $response.more_available
+    $members = $response.users
+
+    while ($hasNext) {
+        $page++
+        $queryParams = "page=$($page)"
+        $uri = "https://www.yammer.com/api/$apiVersion/$($Resource)?$queryParams"
+        $response = Invoke-RestAPI -Uri $uri -Headers $authHeader -Method Get -RetryCount 10 -RetryInterval 30
+        $hasNext = $response.more_available
+        $members += $response.users
+    }
+
+    $members
+}
+
+
+#-------- ここからがメイン処理 --------
+
+Write-Verbose "開発者トークンを取得します。"
+$developerToken = Get-DeveloperToken
+$authHeader = @{
+    'Content-Type' = 'application/json'
+    'Authorization' = "Bearer $developerToken"
+}
+
+Write-Verbose "対象の Yammer グループ IDを取得"
+$groupIdsString = Get-AutomationVariable -Name 'YammerGroupIds'
+Write-Verbose "Yammer グループ リスト: $groupIdsString"
+$groupIds = $groupIdsString -split ","
+
+$groupList = @()
+
+foreach ($groupId in $groupIds) {
+	$groupList += Get-GroupInfo -GroupId $groupId
+}
+
+$groupList | ft
