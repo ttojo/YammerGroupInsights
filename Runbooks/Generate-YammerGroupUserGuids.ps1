64617072ec1ed51f18655093b5b4596b879eea5a
Toshio Tojo <toshio.tojo@live.jp>
2019-07-23 00:37:42 +0900

    モジュールと Automation 変数の利用を組み込んだ

--- Runbooks/Generate-YammerGroupUserGuids.ps1
+++ Runbooks/Generate-YammerGroupUserGuids.ps1
@@ -1,129 +1,85 @@
-﻿param (
-    [Parameter(Mandatory = $true)] $WebUri
-)
+﻿#-------------------------------------------------------------------------------------------------------
+#
+# Yammer ユーザーの GUID 一覧を作成
+#
+# Version:        1.0
+# Author:         Toshio Tojo
+# Company Name:   Microsoft Japan
+# Copyright:      (c) 2019 Toshio Tojo, Microsoft Japan. All rights reserved.
+# Creation Date:  2019/7/20
+#
+#-------------------------------------------------------------------------------------------------------
 
-$VerbosePreference = 'Continue'
+# デバッグ時に詳細メッセージを出力する場合は有効にする
+# $VerbosePreference = 'Continue'
 
-function Get-DeveloperToken {
-    $credential = Get-AutomationPSCredential -Name "YammerDeveloper"
-    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($credential.Password)
-    [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
-}
-
-# REST API の呼び出し（リトライあり）
-function Invoke-RestAPI {
-    param (
-        [Parameter(Mandatory = $true)] [Uri] $Uri,
-        [Parameter(Mandatory = $false)] [System.Collections.IDictionary] $Headers,
-        [Parameter(Mandatory = $false)] [Microsoft.PowerShell.Commands.WebRequestMethod] $Method = [Microsoft.PowerShell.Commands.WebRequestMethod]::Get,
-		[Parameter(Mandatory = $false)] [Object] $Body,
-		[Parameter(Mandatory = $false)] [string] $ContentType,
-        [Parameter(Mandatory = $false)] [Int32] $RetryCount = 0,
-        [Parameter(Mandatory = $false)] [Int32] $RetryInterval = 10
-    )
-
-    $completed = $false
-    $retries = 0
-
-    while (-not $completed) {
-        try {
-            $response = Invoke-RestMethod -Uri $uri -Headers $Headers -Method $Method -Body $Body -ContentType $ContentType
-            $completed = $true
-        } catch {
-            $ex = $_.Exception
-
-            if ($ex.Response -ne $null) {
-                
-                if ($retries -lt $RetryCount) {
-                    $sc = [int]$ex.Response.StatusCode.Value__
-                    if (($sc -eq 304) -or ((400 -le $sc) -and ($sc -le 599))) {
-                        $retries++
-                        Write-Verbose "Retry...($retries/$RetryCount)"
-                        Start-Sleep -Seconds $RetryInterval
-                        continue
-                    }
-                }
-                $errorResponse = $ex.Response.GetResponseStream()
-                $reader = New-Object System.IO.StreamReader($errorResponse)
-                $reader.BaseStream.Position = 0
-                $reader.DiscardBufferedData()
-                $responseBody = $reader.ReadToEnd();
-
-                Write-Host "Response content:`n$responseBody" -f Red
-                Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
-            } else {
-                Write-Host "Unhanded exception:`n$ex"
-            }
-
-            #re-throw
-            throw
-        }
-    }
-
-    return $response
-}
-
-
-#-------- ここからがメイン処理 --------
-
+# ローカル ファイルの出力先
 $LocalTargetDirectory = "C:\"
+
+# ファイル名をユニークにするために埋め込む文字列
 $Date = Get-Date -Format "yyyyMMdd-HHmmss"
 
+# ファイル格納先の情報は Automation 変数から取得する
+$ResourceGroupName = Get-AutomationVariable -Name "ResourceGroupName"
+$StorageAccountName = Get-AutomationVariable -Name "StorageAccountName"
+$JsonContainerName = Get-AutomationVariable -Name "JsonContainerName"
+$GetUserGuidServiceUri = Get-AutomationVariable -Name "GetUserGuidServiceUri"
+
+# PowerShell から Azure に接続し、出力先のストレージ アカウントをセットする
 $conn = Get-AutomationConnection -Name "AzureRunAsConnection"
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint | Out-Null
-Set-AzureRmCurrentStorageAccount -ResourceGroupName 'TOTOJO-STU-RG' -StorageAccountName 'yammergroupinsight' | Out-Null
+Set-AzureRmCurrentStorageAccount -ResourceGroupName $ResourceGroupName -StorageAccountName $StorageAccountName | Out-Null
 
+# Yammer ユーザー一覧ファイルを読み込む
 Write-Verbose "ユーザー一覧を一旦ダウンロードする"
 $blobName = "YammerUsers-Current.json"
-Get-AzureStorageBlob -Container "json" -Blob $blobName | Get-AzureStorageBlobContent -Destination $LocalTargetDirectory | Out-Null
+Get-AzureStorageBlob -Container $JsonContainerName -Blob $blobName | Get-AzureStorageBlobContent -Destination $LocalTargetDirectory | Out-Null
 $LocalFile = $LocalTargetDirectory + $blobName
 $userList = Get-Content $LocalFile | ConvertFrom-Json
 
+# Yammer ユーザー GUID 一覧ファイルを読み込む
 Write-Verbose "ユーザー GUID を一旦ダウンロードする"
 $blobName = "YammerUserGuids-Current.json"
-Get-AzureStorageBlob -Container "json" -Blob $blobName | Get-AzureStorageBlobContent -Destination $LocalTargetDirectory | Out-Null
+Get-AzureStorageBlob -Container $JsonContainerName -Blob $blobName | Get-AzureStorageBlobContent -Destination $LocalTargetDirectory | Out-Null
 $LocalFile = $LocalTargetDirectory + $blobName
 $guidList = Get-Content $LocalFile | ConvertFrom-Json
 
+# Yammer ユーザー GUID 一覧はハッシュ テーブルで持つ (高速化のため)
 Write-Verbose "処理高速化のために GUID リストをハッシュで持つ"
 $guidHash = @{}
 $guidList | ForEach-Object {
-	$guidHash.Add($_.id, [PSCustomObject]@{
-		id = $_.id
-		email = $_.email
-		guid = $_.guid
-	})
+	$guidHash.Add($_.id, [PSCustomObject]@{id = $_.id; email = $_.email; guid = $_.guid})
 }
 
+# Yammer ユーザー GUID 一覧の入れ物
 $results = @()
 
+# Yammer ユーザー GUID 一覧に含まれない Yammer ユーザーの GUID を追加する
 $userList | ForEach-Object {
 	$user = $_
 
 	if ($guidHash.ContainsKey($user.id)) {
 		$results += $guidHash[$user.id]
 	} else {
+		# 未知の Yammer ユーザーの GUID を Web サービスに問い合わせる
 		$requestHashtable = @{"id" = $user.id; "email" = $user.email}
 		$messagePayLoad = ConvertTo-Json $requestHashtable
 		$messagePayload = [System.Text.Encoding]::UTF8.GetBytes($messagePayload)
 
-		$response = Invoke-RestAPI -Method Post -Uri $WebUri -Body $messagePayload -ContentType "application/json" -RetryCount 10 -RetryInterval 30
+		$response = Invoke-RestAPI -Method Post -Uri $GetUserGuidServiceUri -Body $messagePayload -ContentType "application/json" -RetryCount 10 -RetryInterval 30
 
 		$response.users | ForEach-Object {
-			$newGuid = [PSCustomObject]@{
-				id = $_.id
-				email = $_.email
-				guid = $_.guid
-			}
+			$newGuid = [PSCustomObject]@{id = $_.id; email = $_.email; guid = $_.guid}
 			$guidHash.Add($_.id, $newGuid)
 			$results += $newGuid
 		}
 	}
 }
 
+# Yammer ユーザー GUID 一覧ファイルを作り直す
 Write-Verbose "ユーザー一覧をファイル化する"
 $blobName = "YammerUserGuids-Current.json"
 $LocalFile = $LocalTargetDirectory + "YammerUserGuids_" + $Date + ".json"
 $results | Sort-Object id -Unique | ConvertTo-Json -Depth 10 -Compress | Out-File -Encoding utf8 -FilePath $LocalFile -Force
-Get-AzureStorageBlob -Container "json" -Blob $blobName | Remove-AzureStorageBlob
-Set-AzureStorageBlobContent -File $LocalFile -Container "json" -Blob $blobName | Out-Null
+Get-AzureStorageBlob -Container $JsonContainerName -Blob $blobName | Remove-AzureStorageBlob
+Set-AzureStorageBlobContent -File $LocalFile -Container $JsonContainerName -Blob $blobName | Out-Null
